607e4a96520f66d6d84c758d11a63cb7d16ae955
Karamazov <samuel.silveira01@fatec.gov.sp.br>
2022-10-27 18:28:39 -0300

    apostila01(oracle_cloud): bloco de codigo 2.10.1 exibindo apenas nome, sem iteracao sobre a colecao

--- src/PessoaService.java
+++ src/PessoaService.java
@@ -3,6 +3,9 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -11,9 +14,10 @@ public class PessoaService {
   private HttpClient client = HttpClient.newHttpClient();
   private final String url;
 
-  // O contrutor abaixo esta sendo gerado pela Lombok, inicialiado com a notação de constante acima
+  // O construtor abaixo esta sendo gerado pela Lombok, inicializado com a notação
+  // de constante acima
   // public PessoaService(String url){
-  //   this.url = url;
+  // this.url = url;
   // }
 
   public void listar() {
@@ -25,7 +29,16 @@ public class PessoaService {
 
       // var == HttpResponse <String>
       var response = client.send(request, BodyHandlers.ofString());
-      System.out.println(response.body());
+
+      JSONObject raiz = new JSONObject(response.body());
+      System.out.println(raiz);
+      JSONArray items = raiz.getJSONArray("items");
+      // System.out.println(items);
+      JSONObject primeiro =  items.getJSONObject(0);
+      System.out.println(primeiro);
+      //System.out.println(response.body());
+      String nome = primeiro.getString("nome");
+      System.out.println(nome);
 
     } catch (Exception e) {
       System.out.println(e.getMessage());
